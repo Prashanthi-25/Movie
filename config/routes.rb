892e7d118d7fabce9d90d94610256aa86d467909
Prashanthi-25 <prashanthijayakumar98@gmail.com>
2020-05-07 22:12:12 +0530

    commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+    resources :movies
+    get '/search' => 'movies#search', :as => 'search_movies'
+    get '/filter' => 'movies#filter', :as => 'filter_movies'
+    get 'welcome/index'
+end
